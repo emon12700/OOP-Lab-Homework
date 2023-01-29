d7ee682c3a41dbbc3407f331b09a793e4990234a
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-29 13:10:15 +0600

    Add files via upload

---
+++ SolveOperations.java
@@ -0,0 +1,9 @@
+public class SolveOperations {
+    public static void main(String[] args) {
+        int A = 5, B = 6, C = 7, D = 8;
+        System.out.println("(A * B - C * D) = " + (A * B - C * D));
+        System.out.println("(2A - B + 3D) = " + (2 * A - B + 3 * D));
+        System.out.println("(A^2 + B^2 - C^2 + D^2) = " + (A * A + B * B - C * C + D * D));
+        System.out.println("(A^3 + B - C^2) = " + (A * A * A + B - C * C));
+    }
+}
