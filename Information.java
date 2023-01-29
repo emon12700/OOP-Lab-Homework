b1b8598245c82039cec0903ea7b3e1a84edb87ec
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-29 12:46:21 +0600

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,8 @@
+public class Information {
+    public static void main(String[] args) {
+        String name = "Md. Naimul Hasan", ID = "221-15-5601";
+        String Address = "YKSG-2", Dept = "CSE", fvtGame = "Football";
+        System.out.println("Name: " + name + "\n" + "ID: " + ID + "\n" + "Address: " + Address + "\n" + "Deparnment: "
+                + Dept + "\n" + "Favorite Game: " + fvtGame);
+    }
+}
