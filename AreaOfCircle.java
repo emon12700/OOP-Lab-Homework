900e7cd26bb0fb41be3567f62c719bfcbf72b3ed
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-29 13:10:46 +0600

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,6 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        float redius = 14.30f;
+        System.out.printf("%.2f", (Math.PI * redius * redius), "\n");
+    }
+}
