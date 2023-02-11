7bd2f8fe7cd8a8a1064cd0af3fa3b563ba4e03af
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-02-11 15:31:10 +0600

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+        System.out.print("Enter an operator to claculate (+,-,*,/,%): ");
+        char ch = sc.next().charAt(0);
+        double result = 0.0;
+        switch (ch) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            case '%':
+                result = num1 % num2;
+                break;
+            default:
+                System.out.println("Error! The operator is not correct.");
+        }
+        System.out.println("Result = " + result);
+        sc.close();
+    }
+}
