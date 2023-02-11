6247e1aeddb8e3495210466e0ab417a06f8c60c9
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-02-11 15:52:53 +0600

    Update AreaOfCircle.java

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class AreaOfCircle {
     public static void main(String[] args) {
-        float redius = 14.30f;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the redius: ");
+        double redius = sc.nextDouble();
         System.out.printf("%.2f", (Math.PI * redius * redius), "\n");
+        sc.close();
     }
 }
