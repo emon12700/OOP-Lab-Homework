3665859cd66dc800d12eca0ea7319ccba3c8ac1b
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-02-11 15:30:38 +0600

    Delete Calculator.java

--- Calculator.java
+++
@@ -1,9 +0,0 @@
-public class Calculator {
-    public static void main(String[] args) {
-        int a = 20, b = 5;
-        System.out.println("Summation = " + (a + b));
-        System.out.println("Substraction = " + (a - b));
-        System.out.println("Multiplication = " + (a * b));
-        System.out.println("Substraction = " + (a / b));
-    }
-}
